ff2c0dbb5b89247901b48db726dd21474467dc34
bazs <balazs.harmat07@gmail.com>
2025-09-05 10:19:12 +0200

    szkript

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const telepules = document.getElementById("telepules");
 const iranyitoszam=document.getElementById("iranyitoszam");
 const kozterulet=document.getElementById("kozterulet");
 const hazszam=document.getElementById("hazszam");
+localStorage.setItem("nev", nev)
+localStorage.setItem("telepules", telepules)
+localStorage.setItem("iranyitoszam", iranyitoszam)
+localStorage.setItem("kozter", kozterulet)
+localStorage.setItem("hazszam", hazszam)
+document.getElementById("flex-container").innerHTML=nev+telepules+iranyitoszam+kozterulet+hazszam;
