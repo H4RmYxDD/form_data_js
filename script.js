6aaa140f1974df78d4570a63ef374578b9d06e22
bazs <balazs.harmat07@gmail.com>
2025-09-05 10:24:49 +0200

    meg szkript

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const nev = document.getElementById("nev");
-const telepules = document.getElementById("telepules");
-const iranyitoszam=document.getElementById("iranyitoszam");
-const kozterulet=document.getElementById("kozterulet");
-const hazszam=document.getElementById("hazszam");
-localStorage.setItem("nev", nev)
-localStorage.setItem("telepules", telepules)
-localStorage.setItem("iranyitoszam", iranyitoszam)
-localStorage.setItem("kozter", kozterulet)
-localStorage.setItem("hazszam", hazszam)
-document.getElementById("flex-container").innerHTML=nev+telepules+iranyitoszam+kozterulet+hazszam;
+document.getElementById("bekuldes").addEventListener("click", mentes);
+function mentes() {
+  const nev = document.getElementById("nev");
+  const telepules = document.getElementById("telepules");
+  const iranyitoszam = document.getElementById("iranyitoszam");
+  const kozterulet = document.getElementById("kozterulet");
+  const hazszam = document.getElementById("hazszam");
+  localStorage.setItem("nev", nev);
+  localStorage.setItem("telepules", telepules);
+  localStorage.setItem("iranyitoszam", iranyitoszam);
+  localStorage.setItem("kozter", kozterulet);
+  localStorage.setItem("hazszam", hazszam);
+  document.getElementById("flex-container").innerHTML =
+    nev + telepules + iranyitoszam + kozterulet + hazszam;
+}
+document.getElementById("delete").addEventListener("click", torles);
+function torles(){
+    localStorage.clear;
+}
