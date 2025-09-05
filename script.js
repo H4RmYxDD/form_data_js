5e042de551ec6f8f49b1633f7bdf6f8397325e81
bazs <balazs.harmat07@gmail.com>
2025-09-05 10:09:38 +0200

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const nev = document.getElementById("nev");
+const telepules = document.getElementById("telepules");
+const iranyitoszam=document.getElementById("iranyitoszam");
+const kozterulet=document.getElementById("kozterulet");
+const hazszam=document.getElementById("hazszam");
